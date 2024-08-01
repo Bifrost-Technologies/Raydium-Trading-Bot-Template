43cc819015cb744e087e4eb997dc276a2fb6eeb2
Nathan Martell <projectbifrost@gmail.com>
2024-08-01 19:16:17 -0400

    Add custom token amount & fixed bugs with clmm fetching
    Added custom token amount and fixed a bug where fetching CLMM list caused it to loop and spam errors.

--- src/raydiumBotClient.ts
+++ src/raydiumBotClient.ts
@@ -1,260 +1,245 @@
-import { Connection, Keypair, PublicKey, GetProgramAccountsFilter, LAMPORTS_PER_SOL } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID } from "@solana/spl-token";
-
-import bs58 from "bs58"
-
 import {
-    ApiClmmPoolsItem,
-    Clmm,
-    TradeV2,
-    Currency,
-    CurrencyAmount,
-    Percent,
-    Token,
-    TokenAmount,
-    fetchMultipleMintInfos,
-} from '@raydium-io/raydium-sdk';
+  Connection,
+  Keypair,
+  PublicKey,
+  GetProgramAccountsFilter,
+  LAMPORTS_PER_SOL,
+  ComputeBudgetProgram,
+} from '@solana/web3.js'
+import { getOrCreateAssociatedTokenAccount, TOKEN_2022_PROGRAM_ID, TOKEN_PROGRAM_ID } from '@solana/spl-token'
+
+import bs58 from 'bs58'
 
 import {
-    DEFAULT_TOKEN,
-    makeTxVersion,
-    PROGRAMIDS,
-} from '../config';
-import { formatAmmKeysById } from './formatAmmKeysById';
-import {
-    buildAndSendTx,
-    getWalletTokenAccount,
-} from './util';
-import { formatClmmKeys } from "./formatClmmKeys";
-import BN from "bn.js";
+  ApiClmmPoolsItem,
+  Clmm,
+  TradeV2,
+  Currency,
+  CurrencyAmount,
+  Percent,
+  Token,
+  TokenAmount,
+  fetchMultipleMintInfos,
+  publicKey,
+  MAINNET_PROGRAM_ID,
+} from '@raydium-io/raydium-sdk'
+
+import { DEFAULT_TOKEN, makeTxVersion, PROGRAMIDS } from '../config'
+import { formatAmmKeysById } from './formatAmmKeysById'
+import { buildAndSendTx, getWalletTokenAccount } from './util'
+import { formatClmmKeys } from './formatClmmKeys'
+import BN from 'bn.js'
 
 type WalletTokenAccounts = Awaited<ReturnType<typeof getWalletTokenAccount>>
 type TxInputInfo = {
-    inputToken: Token | Currency
-    outputToken: Token | Currency
-    inputTokenAmount: TokenAmount | CurrencyAmount
-    slippage: Percent
-    walletTokenAccounts: WalletTokenAccounts
-    wallet: Keypair
+  inputToken: Token | Currency
+  outputToken: Token | Currency
+  inputTokenAmount: TokenAmount | CurrencyAmount
+  slippage: Percent
+  walletTokenAccounts: WalletTokenAccounts
+  wallet: Keypair
 
-    feeConfig?: {
-        feeBps: BN,
-        feeAccount: PublicKey
-    }
+  feeConfig?: {
+    feeBps: BN
+    feeAccount: PublicKey
+  }
 }
-export enum BuyType{
-    SOL,
-    USDC
+export enum CurrencyType {
+  SOL,
+  USDC,
 }
-export enum TradeSide{
-    Buy,
-    Sell
+export enum TradeSide {
+  Buy,
+  Sell,
 }
-export async function TradeToken(rpcProvider: string, botWallet: Keypair, tokenAddress: string, poolAddress: string, tokenSymbol: string, tokenDecimals: 9, buyType: BuyType, tradeSide: TradeSide)
-{
-    const SolClient = new Connection(rpcProvider);
-    const BotAddress = botWallet.publicKey;
-    const TokenAddress = new PublicKey(tokenAddress);
-    const PoolAddress = new PublicKey(poolAddress);
-    const TargetToken = new Token(TOKEN_PROGRAM_ID, TokenAddress, tokenDecimals, tokenSymbol, tokenSymbol);
+export async function TradeToken(
+  rpcProvider: string,
+  botWallet: Keypair,
+  amount: number,
+  tokenAddress: string,
+  poolAddress: string,
+  tokenDecimals: 9,
+  currencyType: CurrencyType,
+  tradeSide: TradeSide
+) {
+  const SolClient = new Connection(rpcProvider)
+  const BotAddress = botWallet.publicKey
+  const TokenAddress = new PublicKey(tokenAddress)
+  const TargetToken = new Token(TOKEN_PROGRAM_ID, TokenAddress, tokenDecimals)
+  const buyOrderSOLAmount = new CurrencyAmount(DEFAULT_TOKEN.SOL, amount, false)
+  const buyOrderUSDCAmount = new CurrencyAmount(DEFAULT_TOKEN.USDC, amount, false)
+  const sellOrderTokenAmount = new TokenAmount(TargetToken, amount, false)
+  const slippage = new Percent(1, 100)
+  const botTokenAccounts = await getWalletTokenAccount(SolClient, BotAddress)
+  await delay(1000)
 
-    const SolBalance = await SolClient.getBalance(BotAddress) / LAMPORTS_PER_SOL;
-    const USDCbalance = await getUSDCbalance(botWallet, SolClient);
-    await delay(1000);
-    //Retrieve token balance of our target token
-    var TokenBalance = 0;
-    const tokenAccount = await getOrCreateAssociatedTokenAccount(SolClient, botWallet, TokenAddress, BotAddress);
-    try {
-        var balance = await SolClient.getTokenAccountBalance(tokenAccount.address);
-        if (balance.value.uiAmount != null) {
-            TokenBalance = balance.value.uiAmount;
-        }
-    } catch (error) {
-      //error is thrown when no token account exists. The error will go away after you purchase the token for the first time
-        console.log(error);
+  console.log('Amount: ' + amount)
 
-    }
-
-    console.log('SOL Balance: ' + SolBalance);
-    console.log('USDC Balance: ' + USDCbalance);
-    console.log('Token Balance: ' + TokenBalance);
-
-    //Change buy amount to what ever you want
-    var solbuyamount = SolBalance / 5;
-    var usdcbuyamount = USDCbalance / 5;
-    const buyOrderSOLAmount = new CurrencyAmount(DEFAULT_TOKEN.SOL, solbuyamount, false);
-    const buyOrderUSDCAmount = new TokenAmount(DEFAULT_TOKEN.USDC, usdcbuyamount, false);
-    const sellOrderTokenAmount = new TokenAmount(TargetToken, TokenBalance)
-    const slippage = new Percent(1, 100);
-    const botTokenAccounts = await getWalletTokenAccount(SolClient, BotAddress);  
-    await delay(1000);
- 
-    console.log("Attempting to buy token...");
-    console.log("BuyOrder SOL Amount: " + buyOrderSOLAmount.raw);
-    console.log("BuyOrder USDC Amount: " + buyOrderSOLAmount.raw);
-    console.log("SellOrder Token Amount: "+ sellOrderTokenAmount.raw);
-
-    try
-    {
-            console.log("Attempting Trade....");
-            if(tradeSide == TradeSide.Buy){
-
-            
-            if(buyType == BuyType.SOL){
-
-            await Trade(SolClient, PoolAddress.toBase58(), {
-                outputToken: TargetToken,
-                inputToken: DEFAULT_TOKEN.SOL,
-                inputTokenAmount: buyOrderSOLAmount,
-                slippage,
-                walletTokenAccounts: botTokenAccounts,
-                wallet: botWallet,
-            }).then(({ txids }) => {
-                console.log('txids', txids);
-            })
-            }
-            //USDC version of the trade. Make sure to switch pool address to USDC raydium pool
-            if(buyType == BuyType.USDC){
-
-            
-            await Trade(SolClient, PoolAddress.toBase58(), {
-              outputToken: TargetToken,
-              inputToken: DEFAULT_TOKEN.USDC,
-              inputTokenAmount: buyOrderUSDCAmount,
-              slippage,
-              walletTokenAccounts: botTokenAccounts,
-              wallet: botWallet,
-          }).then(({ txids }) => {
-             console.log('txids', txids)
-          })
-        }
-    }
-    if(tradeSide == TradeSide.Sell){
-        //Bot dumps all the tokens it has
-        await Trade(SolClient, PoolAddress.toBase58(), {
-            outputToken: DEFAULT_TOKEN.SOL,
-            inputToken: TargetToken,
-            inputTokenAmount: sellOrderTokenAmount,
-            slippage,
-            walletTokenAccounts: botTokenAccounts,
-            wallet: botWallet,
+  try {
+    console.log('Attempting Trade....')
+    if (tradeSide == TradeSide.Buy) {
+      if (currencyType == CurrencyType.USDC) {
+        await Trade(SolClient, poolAddress, {
+          outputToken: TargetToken,
+          inputToken: DEFAULT_TOKEN.SOL,
+          inputTokenAmount: buyOrderUSDCAmount,
+          slippage,
+          walletTokenAccounts: botTokenAccounts,
+          wallet: botWallet,
         }).then(({ txids }) => {
-           console.log('txids', txids)
+          console.log('txids', txids)
         })
+      }
+      if (currencyType == CurrencyType.SOL) {
+        await Trade(SolClient, poolAddress, {
+          outputToken: TargetToken,
+          inputToken: DEFAULT_TOKEN.SOL,
+          inputTokenAmount: buyOrderSOLAmount,
+          slippage,
+          walletTokenAccounts: botTokenAccounts,
+          wallet: botWallet,
+        }).then(({ txids }) => {
+          console.log('txids', txids)
+        })
+      }
     }
-        await delay(2000);
-       
-      
-    }  
-    catch(e)
-    {
-      console.log(e);
+    if (tradeSide == TradeSide.Sell) {
+      if (currencyType == CurrencyType.SOL) {
+        await Trade(SolClient, poolAddress, {
+          outputToken: DEFAULT_TOKEN.SOL,
+          inputToken: TargetToken,
+          inputTokenAmount: sellOrderTokenAmount,
+          slippage,
+          walletTokenAccounts: botTokenAccounts,
+          wallet: botWallet,
+        }).then(({ txids }) => {
+          console.log('txids', txids)
+        })
+      }
+      if (currencyType == CurrencyType.USDC) {
+        await Trade(SolClient, poolAddress, {
+          outputToken: DEFAULT_TOKEN.USDC,
+          inputToken: TargetToken,
+          inputTokenAmount: sellOrderTokenAmount,
+          slippage,
+          walletTokenAccounts: botTokenAccounts,
+          wallet: botWallet,
+        }).then(({ txids }) => {
+          console.log('txids', txids)
+        })
+      }
+      await delay(1000)
     }
-     
+  } catch (e) {
+    console.log(e)
+  }
 }
 export async function Trade(connect: Connection, Pool: string, input: TxInputInfo) {
-    try {
-
-
-        // -------- pre-action: fetch Clmm pools info and ammV2 pools info --------
-        const clmmPools: ApiClmmPoolsItem[] = await formatClmmKeys(PROGRAMIDS.CLMM.toString(), connect) // If the clmm pool is not required for routing, then this variable can be configured as undefined
-        const clmmList = Object.values(
-            await Clmm.fetchMultiplePoolInfos({ connection: connect, poolKeys: clmmPools, chainTime: new Date().getTime() / 1000 })
-        ).map((i) => i.state)
-        delay(1000);
-
-        const targetPool = Pool;
-        const targetPoolInfo = {
-            official: [], unOfficial: [await formatAmmKeysById(targetPool, connect)],
-
-        }
-        // If the Liquidity pool is not required for routing, then this variable can be configured as undefined
-        console.log("Collected AMM Key info..")
-        // -------- step 1: get all route --------
-        const getRoute = TradeV2.getAllRoute({
-            inputMint: input.inputToken instanceof Token ? input.inputToken.mint : PublicKey.default,
-            outputMint: input.outputToken instanceof Token ? input.outputToken.mint : PublicKey.default,
-            clmmList: clmmList,
-            apiPoolList: targetPoolInfo
-        })
-        console.log("Route retrieved!")
-        // -------- step 2: fetch tick array and pool info --------
-        const [tickCache, poolInfosCache] = await Promise.all([
-            await Clmm.fetchMultiplePoolTickArrays({ connection: connect, poolKeys: getRoute.needTickArray, batchRequest: true }),
-            await TradeV2.fetchMultipleInfo({ connection: connect, pools: getRoute.needSimulate, batchRequest: true }),
-        ])
-        console.log("Collected pool info!")
-        // -------- step 3: calculation result of all route --------
-        const [routeInfo] = TradeV2.getAllRouteComputeAmountOut({
-            directPath: getRoute.directPath,
-            routePathDict: getRoute.routePathDict,
-            simulateCache: poolInfosCache,
-            tickCache,
-            inputTokenAmount: input.inputTokenAmount,
-            outputToken: input.outputToken,
-            slippage: input.slippage,
-            chainTime: new Date().getTime() / 1000, // this chain time
-
-            feeConfig: input.feeConfig,
-
-            mintInfos: await fetchMultipleMintInfos({
-                connection: connect, mints: [
-                    ...clmmPools.map(i => [{ mint: i.mintA, program: i.mintProgramIdA }, { mint: i.mintB, program: i.mintProgramIdB }]).flat().filter(i => i.program === TOKEN_2022_PROGRAM_ID.toString()).map(i => new PublicKey(i.mint)),
-                ]
-            }),
-
-            epochInfo: await connect.getEpochInfo(),
-        })
-        console.log("creating swap transaction...")
-        // -------- step 4: create instructions by SDK function --------
-        const { innerTransactions } = await TradeV2.makeSwapInstructionSimple({
-            routeProgram: PROGRAMIDS.Router,
-            connection: connect,
-            swapInfo: routeInfo,
-            ownerInfo: {
-                wallet: input.wallet.publicKey,
-                tokenAccounts: input.walletTokenAccounts,
-                associatedOnly: true,
-                checkCreateATAOwner: true,
-            },
-
-            computeBudgetConfig: { // if you want add compute instruction
-                units: 400000, // compute instruction
-                microLamports: 1, // fee add 1 * 400000 / 10 ** 9 SOL
-            },
-            makeTxVersion,
-        })
-        console.log("Attempting to send transaction...")
-        return { txids: await buildAndSendTx(input.wallet, connect, innerTransactions) }
-    } catch (error) {
-        console.log(error)
-        const txids: string[] = [];
-        
-        return {
-            txids: txids}
+  try {
+    const targetPool = Pool
+    const targetPoolInfo = {
+      official: [],
+      unOfficial: [await formatAmmKeysById(targetPool, connect)],
     }
+    //const targetPoolInfo = formatAmmKeysToApi(MAINNET_PROGRAM_ID.AmmV4.toString(), connect, false)
+    // If the Liquidity pool is not required for routing, then this variable can be configured as undefined
+    console.log('Collected AMM Key info..')
+    // -------- step 1: get all route --------
+    const getRoute = TradeV2.getAllRoute({
+      inputMint: input.inputToken instanceof Token ? input.inputToken.mint : PublicKey.default,
+      outputMint: input.outputToken instanceof Token ? input.outputToken.mint : PublicKey.default,
+      clmmList: undefined,
+      apiPoolList: await targetPoolInfo,
+    })
+    console.log('Route retrieved!')
+    // -------- step 2: fetch tick array and pool info --------
+    const [tickCache, poolInfosCache] = await Promise.all([
+      await Clmm.fetchMultiplePoolTickArrays({
+        connection: connect,
+        poolKeys: getRoute.needTickArray,
+        batchRequest: true,
+      }),
+      await TradeV2.fetchMultipleInfo({ connection: connect, pools: getRoute.needSimulate, batchRequest: true }),
+    ])
+    console.log('Collected pool info!')
+    // -------- step 3: calculation result of all route --------
+    const [routeInfo] = TradeV2.getAllRouteComputeAmountOut({
+      directPath: getRoute.directPath,
+      routePathDict: getRoute.routePathDict,
+      simulateCache: poolInfosCache,
+      tickCache,
+      inputTokenAmount: input.inputTokenAmount,
+      outputToken: input.outputToken,
+      slippage: input.slippage,
+      chainTime: new Date().getTime() / 1000, // this chain time
+
+      feeConfig: input.feeConfig,
+
+      mintInfos: await fetchMultipleMintInfos({
+        connection: connect,
+        mints: [],
+      }),
+
+      epochInfo: await connect.getEpochInfo(),
+    })
+    console.log('creating swap transaction...')
+    // -------- step 4: create instructions by SDK function --------
+    const { innerTransactions } = await TradeV2.makeSwapInstructionSimple({
+      routeProgram: PROGRAMIDS.Router,
+      connection: connect,
+      swapInfo: routeInfo,
+      ownerInfo: {
+        wallet: input.wallet.publicKey,
+        tokenAccounts: input.walletTokenAccounts,
+        associatedOnly: true,
+        checkCreateATAOwner: true,
+      },
+      computeBudgetConfig: {
+        units: 60000,
+        microLamports: 3,
+      },
+      makeTxVersion,
+    })
+    console.log('Attempting to send transaction...')
+    return {
+      txids: await buildAndSendTx(input.wallet, connect, innerTransactions, {
+        skipPreflight: false,
+        maxRetries: 3,
+        preflightCommitment: 'confirmed',
+      }),
+    }
+  } catch (error) {
+    console.log(error)
+    const txids: string[] = []
+
+    return {
+      txids: txids,
+    }
+  }
 }
 
 export function delay(ms: number) {
-  return new Promise( resolve => setTimeout(resolve, ms) );
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 export async function getUSDCbalance(wallet: Keypair, solanaConnection: Connection) {
-   var TokenBalance = 0; 
-   try {
-        
-        const tokenAccount = await getOrCreateAssociatedTokenAccount(solanaConnection, wallet, new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"), wallet.publicKey);
-   
-        var balance = await solanaConnection.getTokenAccountBalance(tokenAccount.address);
-        if (balance.value.uiAmount != null) {
-            TokenBalance = balance.value.uiAmount;
-            return TokenBalance;
-        }
-    } catch (error) {
-      //error is thrown when no token account exists. The error will go away after you purchase the token for the first time
-        console.log("Token Account may not exist yet - Error Message: "+error);
+  var TokenBalance = 0
+  try {
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+      solanaConnection,
+      wallet,
+      new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
+      wallet.publicKey
+    )
 
+    var balance = await solanaConnection.getTokenAccountBalance(tokenAccount.address)
+    if (balance.value.uiAmount != null) {
+      TokenBalance = balance.value.uiAmount
+      return TokenBalance
     }
-  return TokenBalance;
+  } catch (error) {
+    //error is thrown when no token account exists. The error will go away after you purchase the token for the first time
+    console.log('Token Account may not exist yet - Error Message: ' + error)
+  }
+  return TokenBalance
 }
-
--- src/swapBot.ts
+++ src/swapBot.ts
@@ -1,4 +1,4 @@
-import { TradeToken, BuyType, TradeSide, delay } from './raydiumBotClient';
+import { TradeToken, TradeSide, delay, CurrencyType } from './raydiumBotClient';
 import {  Keypair,  } from "@solana/web3.js";
 import bs58 from "bs58"
 (async () => {
@@ -11,9 +11,9 @@ import bs58 from "bs58"
         const rpcProvider = "https://mainnet.helius-rpc.com/?api-key=YOUROWNKEYHERE";
         const Account = bs58.decode('SECRET KEY GOES HERE');
         var botAccount = Keypair.fromSecretKey(Account);
-        //Replace string variables with the correct token, pool and symbol info. Pool address is the raydium pool address
-        //Switch BuyType to USDC if you plan to use USDC pool
-        await TradeToken(rpcProvider, botAccount, "TOKEN-ADDRESS-HERE", "POOL-ADDRESS-HERE","TOKEN-SYMBOL-HERE", 9, BuyType.SOL, TradeSide.Buy);
+     
+        //Switch CurrencyType to USDC if you plan to use USDC pool
+        await TradeToken(rpcProvider, botAccount, 10000, "TOKEN-ADDRESS-HERE", "POOL-ADDRESS-HERE", 9, CurrencyType.SOL, TradeSide.Buy);
         await delay(1000000000);
          
     }
